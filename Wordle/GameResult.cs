9275766b23fb8a9812c15ba9768a898b8f4eac2b
Kamil <G00433270@atu.ie>
2024-12-26 22:21:12 +0000

    Game Results

--- Wordle/GameResult.cs
+++ Wordle/GameResult.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Wordle
 {
-    class Result
+    public class GameResult
     {
+        public DateTime Timestamp { get; set; }
+        public string Word { get; set; }
+        public int Guesses { get; set; }
+        public string EmojiGrid { get; set; }
+
+        public GameResult(DateTime timestamp, string word, int guesses, string emojiGrid = "")
+        {
+            Timestamp = timestamp;
+            Word = word;
+            Guesses = guesses;
+            EmojiGrid = emojiGrid;
+        }
     }
 }
